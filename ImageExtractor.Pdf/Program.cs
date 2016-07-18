dc552cf112f9f3e921614a2b516ce610bd306445
kkho <khiem_89@hotmail.com>
2016-07-18 20:40:42 +0200

    Add project files.

---
+++ ImageExtractor.Pdf/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageExtractor.Pdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
